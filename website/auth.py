23040fd96a8ca92cfe7e86111ea3bf8193a3b47b
Peter Hessler <peterhessler@hotmail.co.uk>
2022-02-23 11:17:42 +0000

    db func

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign_up')
+@auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+
+    return render_template("sign_up.html")
